67a80ca3e25d4ce3496a03804b4c36ec995da3ed
Tamires <tata_corte@hotmail.com>
2024-02-11 08:52:39 -0300

    Curso de Angular e testes

---
+++ Testes/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -0,0 +1,42 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetComponent } from './like-widget.component';
+
+describe(LikeWidgetComponent.name, () => {
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should auto generate ID during ngOnInit when (@Input id) is not assigned', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+  it('Should NOT auto generate ID during ngOnInit when (@Input id) is assigned', () => {
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  });
+
+  it(`#${LikeWidgetComponent.prototype.like.name} Should trigger (@Output liked) when called`, () => {
+    spyOn(component.liked, 'emit');
+    fixture.detectChanges();
+    component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
+  });
+});
---
+++ curso angular-pt3/gatitobook/src/app/animais/animais.service.ts
@@ -0,0 +1,29 @@
+import { environment } from './../../environments/environment';
+import { TokenService } from './../autenticacao/token.service';
+import { Animais, Animal } from './animais';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const API = environment.apiURL;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnimaisService {
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
+
+  listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`, {
+      headers,
+    });
+  }
+
+  buscaPorID(id: number): Observable<Animal> {
+    const token = this.tokenService.retornaToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<Animal>(`${API}/photos/${id}`, { headers });
+  }
+}
